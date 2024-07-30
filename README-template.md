458afca1a55462e4ac30d8615364a3e31ddf61c1
ToddM <rtoddm@gmail.com>
2024-07-29 20:09:02 -0400

    Completed project including JS Form Validation and all CSS.

---
+++ README-template.md
@@ -0,0 +1,61 @@
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Add their email and submit the form
+- See a success message with their email after successfully submitting the form
+- See form validation messages if:
+  - The field is left empty
+  - The email address is not formatted correctly
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![Desktop Screenshot](/assets/images/newsletter-signup-desktop.png)
+![Mobile Screenshot](/assets/images/newsletter-signup-mobile.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+This was a great project for me to learn and practice JS form validation! One of the things I learned during this project was how to use the Constraint Validation API for form or input validation. Another great improvement was my use of CSS custom properties.
+
+### Continued development
+
+As I continue I plan on focusing on developing my CSS and JS even further.
+
+### Useful resources
+
+- [MDN Web Docs](https://developer.mozilla.org/en-US/) - MDN Web Docs was a great resource for me while working on this project. Specifically their information on JS form validation of CSS custom properties.
+
+## Author
+
+- Website - [GitHub Repot Gallery](https://rtoddm.github.io/git-repo-gallery/)
+- Frontend Mentor - [@rtoddm](https://www.frontendmentor.io/profile/rtoddm)
--- main.js
+++ main.js
@@ -8,70 +8,62 @@ const successPage = document.getElementsByClassName(
 const dismissButton = document.getElementsByClassName("dismiss")[0];
 const input = document.getElementById("email");
 const inputErrorText = document.getElementById("input-error-text");
+const successEmail = document.getElementById("success-message-email");
 
 // ********** Event Listeners ********** //
 
-// ********** Retrieving Form Data Manually ********** //
-// form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-
-//   const data = {};
-//   const fields = e.target.querySelectorAll("input");
-
-//   for (const field of fields) {
-//     data[field.name] = field.value;
-//   }
-
-//   console.log(data);
-
-//   if (window.getComputedStyle(mainPage).display === "flex") {
-//     mainPage.style.display = "none";
-//     successPage.style.display = "flex";
-//   }
-// });
-
-// dismissButton.addEventListener("click", function () {
-//   if (window.getComputedStyle(successPage).display === "flex") {
-//     mainPage.style.display = "flex";
-//     successPage.style.display = "none";
-//   }
-// });
-
-// ********** Retrieving Form Data Using JavaScript's FormData Constructor ********** //
+// ********** RETRIEVING FORM DATA Using JavaScript's FormData Constructor ********** //
 form.addEventListener("submit", function (e) {
   e.preventDefault(e);
-
-  // Done in Two Lines
-  // const formData = new FormData(e.target);
-  // const data = Object.fromEntries(formData);
-
-  //Refactored to One Line
   const data = Object.fromEntries(new FormData(e.target));
-  console.log(data.email);
-  const dataVar = data.email;
+  console.log(data);
+});
 
-  // ********** Data Validation ********** //
-  if (
-    data.email == "" ||
-    data.email.length < 10 ||
-    typeof data.email != "string"
-  ) {
-    input.classList.add("input-error-color");
+// ********** Validating Form Data Using Constraint Validation API  ********** //
+
+//Validating Form Element
+form.addEventListener("submit", function (e) {
+  if (!email.validity.valid) {
+    console.log("This one works!");
+    showError();
     inputErrorText.classList.remove("hidden");
-    inputErrorText.classList.add("input-error-text");
-  } else {
-    //Button Functionality
-    if (window.getComputedStyle(mainPage).display === "flex") {
-      mainPage.style.display = "none";
-      successPage.style.display = "flex";
-    }
+    e.preventDefault();
+  } else if (email.validity.valid) {
+    inputErrorText.classList.add("hidden");
+  }
+});
 
-    dismissButton.addEventListener("click", function () {
-      if (window.getComputedStyle(successPage).display === "flex") {
-        mainPage.style.display = "flex";
-        successPage.style.display = "none";
-        inputErrorText.classList.add("hidden");
-      }
-    });
+// Error Function
+function showError() {
+  if (email.validity.valueMissing) {
+    inputErrorText.textContent = "Please enter an email address.";
+  } else if (email.validity.tooShort) {
+    inputErrorText.textContent = `Email should be at least ${email.minLength} characters; you only entered ${email.value.length} characters.`;
+  } else if (email.validity.typeMismatch) {
+    inputErrorText.textContent = "Entered value needs to be an email address.";
+  } else if (email.validity.tooShort) {
+    inputErrorText.textContent =
+      "Entered value needs to be at least 8 characters long.";
+  }
+}
+
+//Button Functionality
+button.addEventListener("click", function () {
+  if (
+    email.validity.valid &&
+    window.getComputedStyle(mainPage).display === "flex"
+  ) {
+    mainPage.style.display = "none";
+    successPage.style.display = "flex";
+    successEmail.innerHTML = email.value;
+    console.log("It's working:" + email.value);
+  }
+});
+
+dismissButton.addEventListener("click", function () {
+  if (window.getComputedStyle(successPage).display === "flex") {
+    mainPage.style.display = "flex";
+    successPage.style.display = "none";
+    location.reload();
   }
 });
